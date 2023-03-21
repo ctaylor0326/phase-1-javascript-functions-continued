13920c37adda735bbf01c957b1766c6e2ff91d0f
CahenTaylor <cahentaylor@gmail.com>
2023-03-21 11:18:10 -0600

    passed tests

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(message = "roller-skate") {
+  return `This Saturday, I want to ${message}!`;
+}
+
+const mondayWork = function (message = "go to the office") {
+  return `This Monday, I will ${message}.`;
+};
+
+function wrapAdjective(flair = "*") {
+  return function (adj = "crazy") {
+    const withFlair = flair + adj + flair;
+    return `You are ${withFlair}!`;
+  };
+}
